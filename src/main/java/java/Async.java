3a35f3428447a289ff98c7a8d17b38647e7f2e47
Alban Dericbourg <alban@dericbourg.net>
2016-06-23 11:44:47 +0200

    Java example

---
+++ src/main/java/java/Async.java
@@ -0,0 +1,14 @@
+package java;
+
+import java.util.concurrent.Future;
+
+public class Async {
+
+    public Future<Return1> one() {
+        return null;
+    }
+
+    public Future<Return2> two() {
+        return null;
+    }
+}
---
+++ src/main/java/java/Sync.java
@@ -0,0 +1,12 @@
+package java;
+
+public class Sync {
+
+    public Return1 one() {
+        return new Return1();
+    }
+
+    public Return2 two() {
+        return new Return2();
+    }
+}
